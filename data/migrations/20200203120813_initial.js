b802025195df488961b3e248d5ca93c682b56eb9
Victor Tran <victor.n.tran@outlook.com>
2020-02-03 12:38:33 -0500

    create zoos_animal and refs respective table + format doc

--- data/migrations/20200203120813_initial.js
+++ data/migrations/20200203120813_initial.js
@@ -1,29 +1,41 @@
 exports.up = async function(knex) {
-  await knex.schema.createTable('zoos', (table) => {
-    table.increment('id')
-    table.string('name').notNullable()
-    table.string('address').notNullable()
-  })
+  await knex.schema.createTable("zoos", table => {
+    table.increments("id");
+    table.string("name").notNullable();
+    table.string("address").notNullable();
+  });
 
-  await knex.schema.createTable('species', (table) => {
-    table.increment('id');
-    table.string('name').notNullable();
-  })
-  
-  await knex.schema.createTable('animals', (table) => {
-    table.increment('id')
-    table.string('name').notNullable()
-    table.integer('species_id')
+  await knex.schema.createTable("species", table => {
+    table.increments("id");
+    table.string("name").notNullable();
+  });
+
+  await knex.schema.createTable("animals", table => {
+    table.increments("id");
+    table.string("name").notNullable();
+    table
+      .integer("species_id")
       .notNullable()
-      .references('id')
-      .inTable('species');
-  })
+      .references("id")
+      .inTable("species");
+  });
 
-  
+  await knex.schema.createTable("zoos_animals", table => {
+    table
+      .integer("zoo_id")
+      .references("id")
+      .inTable("zoos");
+
+    table
+      .integer('animal_id')
+      .references('id')
+      .inTable('animals')
+  });
 };
 
 exports.down = async function(knex) {
-  await knex.schema.dropTableIfExists('animals')
-  await knex.schema.dropTableIfExists('species')
-  await knex.schema.dropTableIfExists('zoos')
+  await knex.schema.dropTableIfExists("zoos_animals");
+  await knex.schema.dropTableIfExists("animals");
+  await knex.schema.dropTableIfExists("species");
+  await knex.schema.dropTableIfExists("zoos");
 };
