faf0af8760952afa7e70743732bc267bcf7caf61
Victor Tran <victor.n.tran@outlook.com>
2020-02-03 12:13:57 -0500

    add columns for species

--- data/migrations/20200203120813_initial.js
+++ data/migrations/20200203120813_initial.js
@@ -12,7 +12,8 @@ exports.up = async function(knex) {
   })
 
   await knex.schema.createTable('species', (table) => {
-    
+    table.increment('id');
+    table.string('name').notNullable();
   })
 
   
