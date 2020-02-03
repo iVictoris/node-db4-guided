7b55f690b8fb17f52dddaf2f3bcbbe9ffe415c3b
Victor Tran <victor.n.tran@outlook.com>
2020-02-03 12:12:43 -0500

    create columns for zoos

--- data/migrations/20200203120813_initial.js
+++ data/migrations/20200203120813_initial.js
@@ -1,4 +1,18 @@
 exports.up = async function(knex) {
+  await knex.schema.createTable('zoos', (table) => {
+    table.increment('id')
+    table.string('name').notNullable()
+    table.string('address').notNullable()
+  })
+  
+  await knex.schema.createTable('animals', (table) => {
+    
+  })
+
+  await knex.schema.createTable('species', (table) => {
+    
+  })
+
   
 };
 
